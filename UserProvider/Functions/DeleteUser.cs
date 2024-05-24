4d64c03aff03647b9db2aec1087d1754ea224a71
Hanna Grehn <hannagrehn@gmail.com>
2024-05-24 17:51:33 +0200

    done

--- UserProvider/Functions/DeleteUser.cs
+++ UserProvider/Functions/DeleteUser.cs
@@ -5,33 +5,32 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
-namespace UserProvider.Functions
+namespace UserProvider.Functions;
+
+public class DeleteUser
 {
-    public class DeleteUser
+    private readonly ILogger<DeleteUser> _logger;
+    private readonly DataContext _context;
+
+    public DeleteUser(ILogger<DeleteUser> logger, DataContext context)
     {
-        private readonly ILogger<DeleteUser> _logger;
-        private readonly DataContext _context;
-
-        public DeleteUser(ILogger<DeleteUser> logger, DataContext context)
-        {
-            _logger = logger;
-            _context = context;
-        }
-
-        [Function("DeleteUser")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "delete", Route = "users/{id}")] HttpRequestData req, string id)
-        {
-            var user = await _context.Users.FindAsync(id);
-            if (user == null)
-            {
-                return new NotFoundResult();
-            }
-
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
-
-            return new OkResult();
-        }
-
+        _logger = logger;
+        _context = context;
     }
+
+    [Function("DeleteUser")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "delete", Route = "users/{id}")] HttpRequestData req, string id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return new NotFoundResult();
+        }
+
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+
+        return new OkResult();
+    }
+
 }
